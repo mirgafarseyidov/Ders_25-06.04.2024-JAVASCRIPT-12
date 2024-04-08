0d508f5dca11c9f1bcdcba046ee96e507dbe2cb6
mirgafarseyidov <161619722+mirgafarseyidov@users.noreply.github.com>
2024-04-08 19:51:34 +0400

    first commit

---
+++ codetime/app.js
@@ -0,0 +1,23 @@
+
+
+const dropdownElement = document.getElementById("dropdown");
+const dropdownMenu = document.querySelector(".dropdown__mwnu");
+const downIcon = document.getElementById("down__icon")
+const upIcon = document.getElementById("up__icon")
+
+
+
+dropdownElement.addEventListener("click", () = > {
+    dropdownMenu.classList.toggle("show") ;
+    upIcon.classList.toggle("show");
+    downIconIcon.classList.toggle("show");
+
+});
+
+
+
+
+// const menuLink = document.getElementById("menu__link")
+// const dropdown = document.getElementById("dropdown")
+
+//     .show
